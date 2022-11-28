5c98373502a1af6ae33b79627a347529621de65a
rodrigo-dias-silva <rodrigo-cleiton@hotmail.com>
2022-11-28 19:01:28 -0300

    atualização do layout do projeto

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'painel': "url('/images/painel.jpg')"
+      }
+    },
   },
   plugins: [],
 }
